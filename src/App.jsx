9f54f86f9f83c68870c4144cc077b46cc20e6ea1
Samson Dada <57550434+Samson-Dada@users.noreply.github.com>
2023-04-12 12:11:00 +0100

    update working directory

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App(props) {
 
   const [weatherData, setWeatherData] = useState({});
   useEffect(() => {
-    fetch(api)
+    fetch(url)
       .then(response => response.json())
       .then(data => {
         console.log(data);
